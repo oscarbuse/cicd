63ebc4ddf5f1f1a27afc7ab47f00c552e91cd4dc
oscarbuse <oscar@kwalinux.nl>
2020-07-06 18:18:23 +0200

    added text

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ type Server struct{}
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"message": "Hello Git!"}`))
+	w.Write([]byte(`{"message": "Hello from own laptop!"}`))
 }
 
 func main() {
